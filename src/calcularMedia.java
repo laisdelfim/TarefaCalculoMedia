6bc4369480bf128b2e435685b4bc5dea96b54c76
brayan@240211 <laisdelfim@gmail.com>
2024-08-27 07:43:25 -0300

    tarefa-calculo

---
+++ src/calcularMedia.java
@@ -0,0 +1,12 @@
+public class calcularMedia {
+    public static void main(String[] args) throws Exception {
+
+        int Paulo = 10;
+        int Julia = 15;
+        int Carla = 25;
+        int grupo = Paulo + Julia * Carla;
+
+        System.out.println("A nota média do grupo é de : " + grupo);
+    }
+
+}
