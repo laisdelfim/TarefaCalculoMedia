9a8ce55e0f72f4b407513689ae51fac37fda9700
brayan@240211 <laisdelfim@gmail.com>
2024-09-01 19:51:20 -0300

    tarefa-calcular-media

--- src/calcularMedia.java
+++ src/calcularMedia.java
@@ -1,13 +1,12 @@
 public class calcularMedia {
     public static void main(String[] args) throws Exception {
 
-        int primeiraNota = 10;
-        int segundaNota = 15;
-        int terceiraNota = 25;
-        int quartaNota = 8;
+        int n1 = 10;
+        int n2 = 15;
+        int n3 = 25;
+        int n4 = 8;
 
-        int totalMedia = primeiraNota + segundaNota + terceiraNota + quartaNota * 4;
-
+        int totalMedia = (n1 + n2 + n3 + n4) / 4;
         System.out.println("a media do aluno é de: " + totalMedia);
     }
 
